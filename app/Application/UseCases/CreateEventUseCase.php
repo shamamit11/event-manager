d133b49320d119a5a0edc81aa5a007c247e96aac
Amit Sharma <amit.s@ezelink.com>
2024-08-21 15:44:06 +0400

    added event occurence

--- app/Application/UseCases/CreateEventUseCase.php
+++ app/Application/UseCases/CreateEventUseCase.php
@@ -26,12 +26,16 @@ class CreateEventUseCase
         $dateRange = new EventDateRange($startDateTime, $endDateTime);
 
         $recurringPatternObject = null;
+        $occurrences = [];
+
         if ($recurringPattern) {
             if (empty($frequency) || empty($repeat_until)) {
                 throw new \InvalidArgumentException('Frequency and repeat_until must be provided for recurring events.');
             }
             $repeatUntilDateTime = new DateTime($repeat_until);
             $recurringPatternObject = new RecurringPattern($frequency, $repeatUntilDateTime);
+
+            $occurrences = $this->generateOccurrences($title, $description, $dateRange, $recurringPatternObject);
         }
 
         $newEvent = new Event(
@@ -48,6 +52,46 @@ class CreateEventUseCase
             throw new OverlappingEventException("The event overlaps with an existing event.");
         }
 
-        return $this->eventRepository->save($newEvent);
+        $savedEvent = $this->eventRepository->save($newEvent);
+
+        foreach ($occurrences as $occurrence) {
+            $this->eventRepository->save($occurrence);
+        }
+
+        return $savedEvent;
+    }
+
+    private function generateOccurrences(string $title, ?string $description, EventDateRange $dateRange, RecurringPattern $recurringPattern): array
+    {
+        $occurrences = [];
+        $startDateTime = clone $dateRange->getStart();
+        $endDateTime = clone $dateRange->getEnd();
+        $intervalSpec = $this->getDateIntervalSpec($recurringPattern->getFrequency());
+
+        while ($startDateTime <= $recurringPattern->getRepeatUntil()) {
+            $newDateRange = new EventDateRange(clone $startDateTime, clone $endDateTime);
+            $occurrences[] = new Event(null, $title, $description, $newDateRange, null);
+
+            $startDateTime->add(new \DateInterval($intervalSpec));
+            $endDateTime->add(new \DateInterval($intervalSpec));
+        }
+
+        return $occurrences;
+    }
+
+    private function getDateIntervalSpec(string $frequency): string
+    {
+        switch ($frequency) {
+            case 'daily':
+                return 'P1D';
+            case 'weekly':
+                return 'P1W';
+            case 'monthly':
+                return 'P1M';
+            case 'yearly':
+                return 'P1Y';
+            default:
+                throw new \Exception("Invalid frequency: $frequency");
+        }
     }
 }
--- app/Application/UseCases/UpdateEventUseCase.php
+++ app/Application/UseCases/UpdateEventUseCase.php
@@ -8,6 +8,7 @@ use App\Domains\Calendar\ValueObjects\EventDateRange;
 use App\Domains\Calendar\ValueObjects\RecurringPattern;
 use App\Domains\Calendar\Exceptions\OverlappingEventException;
 use DateTime;
+use Illuminate\Support\Facades\DB;
 
 class UpdateEventUseCase
 {
@@ -32,12 +33,16 @@ class UpdateEventUseCase
         $dateRange = new EventDateRange($startDateTime, $endDateTime);
 
         $recurringPatternObject = null;
+        $occurrences = [];
+
         if ($recurringPattern) {
             if (empty($frequency) || empty($repeat_until)) {
                 throw new \InvalidArgumentException('Frequency and repeat_until must be provided for recurring events.');
             }
             $repeatUntilDateTime = new DateTime($repeat_until);
             $recurringPatternObject = new RecurringPattern($frequency, $repeatUntilDateTime);
+
+            $occurrences = $this->generateOccurrences($title, $description, $dateRange, $recurringPatternObject);
         }
 
         $updatedEvent = new Event(
@@ -54,6 +59,59 @@ class UpdateEventUseCase
             throw new OverlappingEventException("The event overlaps with an existing event.");
         }
 
+        DB::transaction(function () use ($updatedEvent, $existingEvent, $occurrences) {
+            // Delete existing occurrences
+            if ($existingEvent->getRecurringPattern()) {
+                $this->eventRepository->deleteById($existingEvent->id);
+            }
+
+            // Save updated event
+            $this->eventRepository->save($updatedEvent);
+
+            // Save new occurrences
+            foreach ($occurrences as $occurrence) {
+                $this->eventRepository->save($occurrence);
+            }
+        });
+
         $this->eventRepository->save($updatedEvent);
+
+        foreach ($occurrences as $occurrence) {
+            $this->eventRepository->save($occurrence);
+        }
+    }
+
+    private function generateOccurrences(string $title, ?string $description, EventDateRange $dateRange, RecurringPattern $recurringPattern): array
+    {
+        $occurrences = [];
+        $startDateTime = clone $dateRange->getStart();
+        $endDateTime = clone $dateRange->getEnd();
+        $intervalSpec = $this->getDateIntervalSpec($recurringPattern->getFrequency());
+
+        while ($startDateTime <= $recurringPattern->getRepeatUntil()) {
+            $newDateRange = new EventDateRange(clone $startDateTime, clone $endDateTime);
+            $occurrences[] = new Event(null, $title, $description, $newDateRange, null);
+
+            $startDateTime->add(new \DateInterval($intervalSpec));
+            $endDateTime->add(new \DateInterval($intervalSpec));
+        }
+
+        return $occurrences;
+    }
+
+    private function getDateIntervalSpec(string $frequency): string
+    {
+        switch ($frequency) {
+            case 'daily':
+                return 'P1D';
+            case 'weekly':
+                return 'P1W';
+            case 'monthly':
+                return 'P1M';
+            case 'yearly':
+                return 'P1Y';
+            default:
+                throw new \Exception("Invalid frequency: $frequency");
+        }
     }
 }
